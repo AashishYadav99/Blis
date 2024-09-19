d0d1495bfc767f46feea32fdbb9f3d56279038f5
vallabhiAshishYadav <ashish.yadav@vallabhiindia.com>
2024-09-19 12:02:32 +0530

    Apk link updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,14 +11,14 @@ const Home = () => {
       <div className="flex justify-between ">
         <div>
           <Link
-            to="https://drive.google.com/drive/folders/1ICYUMt0tUSKh2hGltU2pLQV9J8MabEFD?usp=drive_link
-"
+            to="https://imagesvideoapp.s3.ap-southeast-1.amazonaws.com/app-release(Blis).apk
+ "
           >
             <button
               type="button"
               className="text-white bg-red-700 hover:bg-red-500 focus:ring-2 focus:ring-white font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex items-center space-x-2"
             >
-              {" "}
+           
               Download APK
             </button>
           </Link>
